31a239dbea2df234704cb004e876a6a224c12bb2
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-17 16:11:43 +0100

    api store

--- app/Http/Controllers/Api/V1/SkillController.php
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSkillRequest;
+use App\Models\Skill;
 use Illuminate\Http\Request;
 
 class SkillController extends Controller
 {
     public function index(){
-        return response() -> json('Skill List');
+        $skills = Skill::all();
+        return response() -> json($skills);
+    }
+    public function store(StoreSkillRequest $request){
+        Skill::create($request->validated());
+        return response() ->json('Skill Created');
     }
 }
