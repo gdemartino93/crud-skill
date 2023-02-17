ddb75bdab9ec439bb688948af1614e5e17cfab29
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-17 23:46:51 +0100

    create view to setup single page app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,9 +7,9 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <div class="wrapper">
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="d-flex justify-content-center align-items-center gap-5">
+        <RouterLink to="/" class="bg-black bg-gradient fw-bold text-white p-3 rounded-pill ">Home</RouterLink>
+        <RouterLink to="/skills" class="bg-black bg-gradient fw-bold text-white p-3 rounded-pill ">Skills</RouterLink>
       </nav>
     </div>
   </header>
