46c81bfef3126a2cc5d6d7082cf1f85c80cf3b3e
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-02-18 13:06:28 +0100

    added audio on chat open click

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,99 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import { onMounted } from 'vue';
+
+onMounted(()=>{
+  // FORM PER LA CHAT A COMPARSA
+  const TASTOCHAT = document.getElementById('chatBtn');
+  const CLOSECHAT = document.getElementById('closeChat');
+  const CHAT = document.getElementById('chat');
+  const BODYCHAT = document.querySelector('.body-chat');
+
+  function closeChat(){
+    if (CHAT.classList.contains('d-block')){
+      CHAT.className = 'd-none'
+      document.title = "Portfolio"
+    }else{
+      CHAT.className = 'd-block'
+      document.title = "Invia messaggio"
+      CREATEAUDIO.load();
+      CREATEAUDIO.play();
+    };
+  }
+  CHAT.className= 'd-none';
+  TASTOCHAT.addEventListener('click', closeChat);
+  CLOSECHAT.addEventListener('click',closeChat);
+
+  const INPUTBODYCHAT = BODYCHAT.querySelectorAll('input');
+  for (let i = 0; i < INPUTBODYCHAT.length; i++) {
+     INPUTBODYCHAT[i].style.border = ' 5px solid #198754';
+  }
+    // FORM PER LA CHAT A COMPARSA FINE
+
+    // esegui audio quando apri la chat
+    const CREATEAUDIO = document.createElement('audio');
+    CREATEAUDIO.setAttribute('id', 'audio');
+    CREATEAUDIO.setAttribute('src','https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-84577/zapsplat_multimedia_button_click_bright_003_92100.mp3')
+
+
+})
+
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
+  <header class="d-flex align-items-center justify-content-center ">
 
       <nav class="d-flex justify-content-center align-items-center gap-5">
-        <RouterLink to="/" class="bg-black bg-gradient fw-bold text-white p-3 rounded-pill ">Home</RouterLink>
-        <RouterLink to="/skills" class="bg-black bg-gradient fw-bold text-white p-3 rounded-pill ">Skills</RouterLink>
+        <RouterLink to="/" class="bg-black bg-gradient fw-bold text-white p-3 rounded-pill" >
+          <span id="home">Home</span>
+        </RouterLink>
+        <RouterLink to="/skills" class="bg-black bg-gradient fw-bold text-white p-3 rounded-pill">
+          <span id="home-btn">Skills</span>
+        </RouterLink>
+        <RouterLink to="" class="bg-black bg-gradient fw-bold text-white p-3 rounded-pill">
+          <span id="chatBtn">Contact me</span>
+        </RouterLink>
+        
       </nav>
-    </div>
   </header>
-
+  <!-- modulo invia messaggio -->
+  <section id="chat" class="">
+    <div class="header-chat">
+      <span id="closeChat">X</span>
+    </div>
+    <div class="body-chat p-2 d-flex flex-column">
+      <label for="email">Inserisci la tua email:</label>
+      <input type="text">
+      <label for="object">Inserisci l'oggetto del messaggio:</label>
+      <input type="text">
+      <label for="text">Scrivi il messaggio:</label>
+      <input type="text" id="areaTesto">
+    </div>
+    <div class="body-cmd d-flex justify-content-end">
+      <button class="btn btn-success">Invia</button>
+    </div>
+  </section>
   <RouterView />
 </template>
+
+<style lang="scss" scoped>
+header{
+  height: 20vh;
+}
+#chat{
+  width: 400px;
+  padding: 10px;
+  background-color: grey;
+  position: absolute;
+  bottom: 0;
+  right: 50px;
+  border: 5px solid black;
+  z-index: 1;
+  border-radius: 10px;
+}
+#areaTesto{
+  height: 200px;
+  border: 3px solid #198754;
+}
+</style>
 
